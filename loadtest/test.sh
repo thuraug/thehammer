0515716369e33af172bd23b6cee6f63adb81421b
thuraug <78434868+thuraug@users.noreply.github.com>
2021-02-03 11:05:54 -0800

    Updated Test.sh with checks

--- loadtest/test.sh
+++ loadtest/test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 loadType=''
+storageSystem=''
+tier=''
+pathToResults=''
+resultsDirectory=''
 
 for ((i=1; i<=`wc -l < Config_File.txt`; i++))
 do
@@ -13,19 +17,48 @@ do
 			loadType=`echo ${line:9} | tr '[:upper:]' '[:lower:]'`
 			echo $loadType
 		else
+			figlet 'ERROR'
 			echo 'LOADTYPE does not have a correct option. Please correct with either "frametest" or "fio"'
-			echo "error is on line ${i} here: ${line}"
+			echo "Error is on line ${i} here: ${line}"
 			exit
 		fi
 	elif [ "${line:0:11}" == "STORAGETYPE" ]
 	then
-		echo ${line:12}
+		if [[ `echo "${line:12}" | tr '[:upper:]' '[:lower:]'` == "gpfs" || `echo "${line:12}" | tr '[:upper:]' '[:lower:]'` == "vast" ]]
+		then
+			storageSystem=`echo "${line:12}" | tr '[:upper:]' '[:lower:]'`
+			echo $storageSystem
+		else
+			figlet 'ERROR'
+			echo 'STORAGETYPE does not have a correct option. Please correct with either "gpfs" or "vast"'
+			echo "Error is on line ${i} here: ${line}"
+		fi
 	elif [ "${line:0:4}" == "TIER" ]
 	then
-		echo ${line:5}
+		if [[ `echo "${line:5}" | tr '[:lower:]' '[:upper:]'` == "NVME" || `echo "${line:5}" | tr '[:lower:]' '[:upper:]'` == "NLSAS" || `echo "${line:5}" | tr '[:lower:]' '[:upper:]'` == "SAS" ]]
+		then
+			tier=`echo "${line:5}" | tr '[:lower:]' '[:upper:]'`
+			echo $tier
+		else
+			figlet 'ERROR'
+			echo 'TIER does not have a correct option. Please correct with either "NVME", "NLSAS" or "SAS"'
+			echo "Error is on line ${i} here: ${line}"
+		fi
+		
 	elif [ "${line:0:16}" == "RESULTSDIRECTORY" ]
 	then
-		echo ${line:17}
+		[ ! -d ${line:17} ] && mkdir ${line:17}
+
+		if [ "${line:-1}" == "/" ]
+		then
+			resultsDirectory="${line:17}"
+		else
+			resultsDirectory="${line:17}/"
+		fi
+
+		pathToResults=${resultsDirectory}LOAD_TEST_RESULTS/
+		
+		echo $pathToResults
 	fi
 
 	
