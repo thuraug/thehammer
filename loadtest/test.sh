e2f74cf7e843c103f13da7085fd58bc1ece29c1f
thuraug <78434868+thuraug@users.noreply.github.com>
2021-02-03 10:40:47 -0800

    Update test.sh

--- loadtest/test.sh
+++ loadtest/test.sh
@@ -6,21 +6,23 @@ for ((i=1; i<=`wc -l < Config_File.txt`; i++))
 do
 	line=`sed -n ${i}p Config_File.txt`
 	
-	case $line in
-		"${line:0:8}" == "LOADTYPE")
-			echo ${line:9}
-			;;
-		loadType=${line:9}
-		"${line:0:11}" == "STORAGETYPE")
-			echo ${line:12}
-			;;
-		"${line:0:4}" == "TIER")
-			echo ${line:5}
-			;;
-		"${line:0:16}" == "RESULTSDIRECTORY")
-			echo ${line:17}
-			;;
-	esac
+	if [ "${line:0:8}" == "LOADTYPE" ]
+	then
+		echo ${line:9}
+		if [ `echo "${line:9}" | tr '[:upper:]' '[:lower:]'` == "frametest" || `echo "${line:9}" | tr '[:upper:]' '[:lower:]'` == "fio"]
+		then
+			loadType=${line:9}
+		fi
+	elif [ "${line:0:11}" == "STORAGETYPE" ]
+	then
+		echo ${line:12}
+	elif [ "${line:0:4}" == "TIER" ]
+	then
+		echo ${line:5}
+	elif [ "${line:0:16}" == "RESULTSDIRECTORY" ]
+	then
+		echo ${line:17}
+	fi
 
 	
 	if [ "$loadType" == "fio" ]
