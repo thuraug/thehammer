b47f77e64113da7570708d53d65326d2ef6b4287
thuraug <78434868+thuraug@users.noreply.github.com>
2021-04-12 14:59:35 -0700

    Clean Up Folder

--- loadtest/submit.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$data_file = fopen("text.txt", "w");
-	$storage = $_POST["storage"];
-	$loadType = $_POST["loadType"];
-	$location = $_POST["location"];
-	$text_to_write = $storage . " " . $loadType;
-	fwrite($data_file, $text_to_write);
-	fclose($data_file);
-?>
